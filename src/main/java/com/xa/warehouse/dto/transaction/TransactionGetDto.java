7a02acf1e6974106461befcb20daa0d787a07d40
Ismailov770 <ravilismailov770@gmail.com>
2023-10-15 13:43:44 +0500

    new updates

--- src/main/java/com/xa/warehouse/dto/transaction/TransactionGetDto.java
+++ src/main/java/com/xa/warehouse/dto/transaction/TransactionGetDto.java
@@ -2,7 +2,6 @@ package com.xa.warehouse.dto.transaction;
 
 import com.xa.warehouse.dto.GenericDto;
 import com.xa.warehouse.dto.transaction.soldProduct.SoldProductGetDto;
-import com.xa.warehouse.entity.transaction.SoldProduct;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
